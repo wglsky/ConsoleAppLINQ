d346787fb7f204e57766d5d95efd09fe4b73d02a
wglsky <40151406+wglsky@users.noreply.github.com>
2021-09-17 14:27:57 +0200

    Add project files.

---
+++ LINQToArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LINQToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ to Array!");
+            // Data source
+            string[] names = { "Tom", "Jerry", "Bill", "Trump", "Joe", "Obama" };
+            // LIN Query
+
+        }
+    }
+}
